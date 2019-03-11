98cd02c479c7463d7bf332b748657e86d926c948
Alexander Snegovoy <snegovoy@hotmail.com>
2019-03-11 15:43:34 +0200

    test logs

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe ApplicationController, type: :controller do
     context "when auth token is passed" do
       before { allow(request).to receive(:headers).and_return(headers) }
 
+      # private method authorize_request returns current user
       it "sets the current user" do
         expect(subject.instance_eval { authorize_request }).to_not eq(user)
       end
