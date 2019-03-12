cbeec026bfc61b659791a772ea8458ea3fabd362
Alexander Snegovoy <snegovoy@hotmail.com>
2019-03-12 15:08:11 +0200

    test

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,7 +1,6 @@
 require "rails_helper"
 
 RSpec.describe ApplicationController, type: :controller do
-  # create test user
   let!(:user) { create(:user) }
    # set headers for authorization
   let(:headers) { { 'Authorization' => token_generator(user.id) } }
